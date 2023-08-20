dde6985924d1663e72ce8624c6ab4d38a3284245
Andrew Alfred <andrewalfred99@yahoo.com>
2023-08-20 15:22:50 +0300

    Switching to API

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -17,13 +18,29 @@ type User struct {
 }
 
 func main() {
-
 	dsn := "host=localhost user=postgres password=Victor@1939 dbname=Task1 port=5432 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	db.AutoMigrate(&User{})
 	if err != nil {
 		panic("failed to connect database")
 	}
+
+	r := gin.Default()
+	r.GET("/Posts", Posts)
+	r.POST("/Store", Store)
+	r.PUT("/Update", Update)
+	r.DELETE("/Delete", Delete)
+	r.GET("/Show", Show)
+	r.Run() // listen and serve on
+
+}
+func Posts(c *gin.Context)  {}
+func Store(c *gin.Context)  {}
+func Update(c *gin.Context) {}
+func Delete(c *gin.Context) {}
+func Show(c *gin.Context)   {}
+func Task() {
+
 	println("Please Choose TO create User press 1, To Update User press 2, To Delete User press 3, To Get User press 4")
 	var choice int
 	fmt.Scanln(&choice)
