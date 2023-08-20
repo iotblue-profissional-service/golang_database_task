9a31ec66675f4d127ce1a82bc044b8eee09ae7f3
Andrew Alfred <andrewalfred99@yahoo.com>
2023-08-20 13:11:10 +0300

    Create, Update, Delete functions and configrations

--- main.go
+++ main.go
@@ -1,18 +1,68 @@
 package main
 
-package main
-
 import (
-  "gorm.io/gorm"
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-type Product struct {
-  gorm.Model
-  Name string
-  Email string
-  Paswword string
-  Age int
+type User struct {
+	gorm.Model
+	Name     string
+	Email    string
+	Password string
+	Age      int
+	ID       int
 }
+
 func main() {
-	
+
+	dsn := "host=localhost user=postgres password=Victor@1939 dbname=Task1 port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db.AutoMigrate(&User{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	println("Please Choose TO create User press 1, To Update User press 2, To Delete User press 3, To Get User press 4")
+	var choice int
+	fmt.Scanln(&choice)
+	if choice == 1 {
+		println("Enter Name")
+		var name string
+		fmt.Scanln(&name)
+		println("Enter Email")
+		var email string
+		fmt.Scanln(&email)
+		println("Enter Password")
+		var password string
+		fmt.Scanln(&password)
+		println("Enter Age")
+		var age int
+		fmt.Scanln(&age)
+		db.Create(&User{Name: name, Email: email, Password: password, Age: age})
+	}
+
+	if choice == 2 {
+		println("Enter Name")
+		var name string
+		fmt.Scanln(&name)
+		println("Enter Email")
+		var email string
+		fmt.Scanln(&email)
+		println("Enter Password")
+		var password string
+		fmt.Scanln(&password)
+		println("Enter Age")
+		var age int
+		fmt.Scanln(&age)
+		db.Model(&User{}).Where("name = ?", name).Updates(User{Name: name, Email: email, Password: password, Age: age})
+	}
+
+	if choice == 3 {
+		println("Enter Name")
+		var name string
+		fmt.Scanln(&name)
+		db.Where("name = ?", name).Delete(&User{})
+	}
 }
